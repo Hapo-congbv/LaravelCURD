cc1fa794d4e6c5624fd61720cf97db3bdbc0e373
hapo-congbv <cong.bv@haposoft.com>
2020-08-14 16:04:44 +0700

    update

--- app/Http/Requests/UsersRequestUpdate.php
+++ app/Http/Requests/UsersRequestUpdate.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UsersRequest extends FormRequest
+class UsersRequestUpdate extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -28,7 +28,7 @@ class UsersRequest extends FormRequest
             'full_name'         => 'required|string|min:4|max:50',
             'mobile'            => 'required',
             'email'             => 'required|email',
-            'user_image'        => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'user_image'        => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'password'          => 'required|min:8'
         ];
     }
@@ -41,7 +41,6 @@ class UsersRequest extends FormRequest
             'mobile.required'       => 'Vui lòng nhập số điện thoại !!',
             'email.required'        => 'Vui lòng nhập email !!',
             'full_name.required'    => 'Full name không được để trống !!',
-            'user_image.required'   => 'Hình ảnh không được để trống !!',
             'password.required'     => 'Vui lòng nhập password !!',
             'password.min'          => 'Password phải chứa ít nhất 8 ký tự !!'
         ];
